0fe667634682a55632b9c628838cf88797339dc0
Mai-xiyu <2546380036@qq.com>
2026-04-14 15:01:25 +0800

    chore: release 1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
     defaultConfig {
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.2.0"
+        versionCode = 9
+        versionName = "1.2.1"
     }
 
     buildTypes {
